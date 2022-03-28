d839aa833479f32c17f69601a2b2b8441ad76b37
fr.Innocent <brother.symeon@gmail.com>
2022-03-28 15:41:59 -0400

    fixed more stuff

--- orderpuller/pull.py
+++ orderpuller/pull.py
@@ -1,10 +1,15 @@
 """code for pulling data from site"""
 
 
-import requests.auth import HTTPBasicAuth 
-
+from requests.auth import HTTPBasicAuth 
+import requests
 
 def getReport():
     # we probably want to get these credentials from a config or something
-    r = requests.post('https:htpbin.org/post', auth=HTTPBasicAuth('innocent', 'blahblah'))
-    return r.data
+    r = requests.post('https://httpbin.org/post', auth=HTTPBasicAuth('innocent', 'blahblah'))
+    return r
+
+
+if __name__ == "__main__":
+    r = getReport()
+    print(r.json)
